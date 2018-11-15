9080b99fb5eed23b16cb8d310c365bbdf1fa4ef5
Chaoyi He <hechaoyi@me.com>
2018-11-14 17:08:31 -0800

    index.html UI & basic data-table

--- src/main/java/imdb/Application.java
+++ src/main/java/imdb/Application.java
@@ -1,19 +1,29 @@
 package imdb;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
+@RequiredArgsConstructor
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
-    @RequestMapping("/")
-    public String home() {
-        return "Hello Docker World";
+    private final JdbcTemplate jdbcTemplate;
+    private final BeanPropertyRowMapper<Movie> movieMapper = BeanPropertyRowMapper.newInstance(Movie.class);
+
+    @RequestMapping("/movies")
+    public List<Movie> movies() {
+        String sql = "SELECT id,title,year FROM movie WHERE ROWNUM<=20";
+        return jdbcTemplate.query(sql, movieMapper);
     }
 }
---
+++ src/main/java/imdb/Movie.java
@@ -0,0 +1,10 @@
+package imdb;
+
+import lombok.Data;
+
+@Data
+public class Movie {
+    private int id;
+    private String title;
+    private int year;
+}
