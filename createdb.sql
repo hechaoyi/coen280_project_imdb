564ed680b4e4308375f69003a458a4fd064a3b2e
Chaoyi He <hechaoyi@me.com>
2018-11-16 11:48:46 -0800

    fix oracle repo & create index

--- createdb.sql
+++ createdb.sql
@@ -1,16 +1,19 @@
 CREATE TABLE movie
 (
-	id                 INTEGER       PRIMARY KEY,
-	title              VARCHAR2(255) NOT NULL,
-	year               INTEGER       NOT NULL,
-	all_critics_rating NUMBER(3,1),
-	top_critics_rating NUMBER(3,1),
-	audience_rating    NUMBER(3,1),
-	all_critics_num    INTEGER,
-	top_critics_num    INTEGER,
-	audience_num       INTEGER,
-	pic_url            VARCHAR2(255)
+  id                 INTEGER       PRIMARY KEY,
+  title              VARCHAR2(255) NOT NULL,
+  year               INTEGER       NOT NULL,
+  all_critics_rating NUMBER(3,1),
+  top_critics_rating NUMBER(3,1),
+  audience_rating    NUMBER(3,1),
+  all_critics_num    INTEGER,
+  top_critics_num    INTEGER,
+  audience_num       INTEGER,
+  pic_url            VARCHAR2(255)
 );
+CREATE INDEX idx_movie__year               ON movie (year);
+CREATE INDEX idx_movie__all_critics_rating ON movie (all_critics_rating);
+CREATE INDEX idx_movie__all_critics_num    ON movie (all_critics_num);
 
 
 
@@ -27,6 +30,8 @@ CREATE TABLE movie_genre
   CONSTRAINT movie_genre__movie__fk FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
   CONSTRAINT movie_genre__genre__fk FOREIGN KEY (genre_id) REFERENCES genre(id) ON DELETE CASCADE
 );
+CREATE INDEX idx_movie_genre__movie_id ON movie_genre (movie_id);
+CREATE INDEX idx_movie_genre__genre_id ON movie_genre (genre_id);
 
 
 
@@ -43,6 +48,8 @@ CREATE TABLE movie_country
   CONSTRAINT movie_country__movie__fk   FOREIGN KEY (movie_id)   REFERENCES movie(id)   ON DELETE CASCADE,
   CONSTRAINT movie_country__country__fk FOREIGN KEY (country_id) REFERENCES country(id) ON DELETE CASCADE
 );
+-- CREATE INDEX idx_movie_country__movie_id   ON movie_country (movie_id);
+CREATE INDEX idx_movie_country__country_id ON movie_country (country_id);
 
 CREATE TABLE movie_location
 (
@@ -54,6 +61,8 @@ CREATE TABLE movie_location
   CONSTRAINT movie_location__movie__fk   FOREIGN KEY (movie_id)   REFERENCES movie(id)   ON DELETE CASCADE,
   CONSTRAINT movie_location__country__fk FOREIGN KEY (country_id) REFERENCES country(id) ON DELETE CASCADE
 );
+CREATE INDEX idx_movie_location__movie_id   ON movie_location (movie_id);
+CREATE INDEX idx_movie_location__country_id ON movie_location (country_id);
 
 
 
@@ -71,6 +80,7 @@ CREATE TABLE movie_tag
   CONSTRAINT movie_tag__movie__fk FOREIGN KEY (movie_id) REFERENCES movie(id) ON DELETE CASCADE,
   CONSTRAINT movie_tag__tag__fk   FOREIGN KEY (tag_id)   REFERENCES tag(id)   ON DELETE CASCADE
 );
+CREATE INDEX idx_movie_tag__movie__tag ON movie_tag (movie_id, tag_id);
 
 
 
